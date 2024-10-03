eb8d910fd79a42ca4c0722b862d882dfbc873de6
Sang Munn Kim <sangk@stud.ntnu.no>
2024-10-03 13:26:56 +0200

    feat: add completedTasks.fxml window and controller

---
+++ kollapp/ui/src/main/java/ui/CompletedTasksController.java
@@ -0,0 +1,23 @@
+package ui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import persistence.ToDoListHandler;
+import persistence.UserHandler;
+import core.User;
+
+public class CompletedTasksController {
+    
+    
+    
+}
