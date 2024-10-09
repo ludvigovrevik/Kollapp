7217d5affe88a3b9c4910616601e59497354450f
Sang Munn Kim <sangk@stud.ntnu.no>
2024-10-09 15:18:58 +0200

    chore: clean up imports

--- kollapp/ui/src/main/java/ui/RegisterController.java
+++ kollapp/ui/src/main/java/ui/RegisterController.java
@@ -2,6 +2,7 @@ package ui;
 
 import java.io.IOException;
 
+import core.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,9 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import persistence.ToDoListHandler;
 import persistence.UserHandler;
-import core.User;
 
 public class RegisterController {
 
