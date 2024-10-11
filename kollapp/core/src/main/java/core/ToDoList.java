658ab6a5e3cffc1434149d3f45ffdc5a0f8541ad
Ludvig Øvrevik <ludvigho@ntnu.no>
2024-10-11 17:39:38 +0200

    docs: add javadoc to core classes

--- kollapp/core/src/main/java/core/ToDoList.java
+++ kollapp/core/src/main/java/core/ToDoList.java
@@ -6,22 +6,35 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
- * Represents a to-do list that contains tasks.
+ * Represents a to-do list that contains multiple tasks.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ToDoList implements java.io.Serializable {
     private List<Task> tasks = new ArrayList<>();
 
+    /**
+     * Default constructor for creating an empty ToDoList.
+     */
     public ToDoList() {
     }
 
+    /**
+     * Constructs a new ToDoList by copying the tasks from another ToDoList.
+     *
+     * @param toDoList the ToDoList to copy
+     */
     public ToDoList(ToDoList toDoList) {
         for (Task task : toDoList.getTasks()) {
             tasks.add(new Task(task));
         }
     }
 
-    // Add a task to the to-do list
+    /**
+     * Adds a task to the to-do list.
+     *
+     * @param newTask the task to add
+     * @throws IllegalArgumentException if the task is null
+     */
     public void addTask(Task newTask) {
         if (newTask == null) {
             throw new IllegalArgumentException("Task cannot be null.");
@@ -29,15 +42,24 @@ public class ToDoList implements java.io.Serializable {
         tasks.add(newTask);
     }
 
-    // Get the list of tasks
+    /**
+     * Returns a copy of the list of tasks.
+     *
+     * @return a list of tasks
+     */
     public List<Task> getTasks() {
         return new ArrayList<>(tasks);
     }
 
-    // Remove a task from the to-do list
+    /**
+     * Removes a task from the to-do list.
+     *
+     * @param task the task to remove
+     * @throws IndexOutOfBoundsException if the task is not in the list
+     */
     public void removeTask(Task task) {
         if (!tasks.contains(task)) {
-            throw new IndexOutOfBoundsException("Index is out of range.");
+            throw new IndexOutOfBoundsException("Task is not in the list.");
         }
         tasks.remove(task);
     }
--- kollapp/core/src/main/java/core/User.java
+++ kollapp/core/src/main/java/core/User.java
@@ -3,49 +3,87 @@ package core;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a user in the system, containing a username, password, 
+ * associated groups, and a to-do list.
+ */
 public class User implements java.io.Serializable {
     private String username;
     private String password;
     private List<String> userGroups = new ArrayList<>();
     private ToDoList toDoList;
 
-    // Default constructor required for Jackson
+    /**
+     * Default constructor required for Jackson deserialization.
+     */
     public User() {
     }
 
-    // Constructor for creating a new user
+    /**
+     * Constructs a new user with a username and password.
+     * Initializes the user's to-do list.
+     *
+     * @param username the username of the user
+     * @param password the password of the user
+     */
     public User(String username, String password) {
         this.username = username;
         this.password = password;
         this.toDoList = new ToDoList();
     }
 
-    // Get the username of the user
+    /**
+     * Returns the username of the user.
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
-    // Get the password of the user
+    /**
+     * Returns the password of the user.
+     *
+     * @return the password
+     */
     public String getPassword() {
         return password;
     }
 
-    // Get the list of user groups
+    /**
+     * Returns a list of the user's groups.
+     *
+     * @return a list of user groups
+     */
     public List<String> getUserGroups() {
         return new ArrayList<>(userGroups);
     }
 
-    // Add a group to the users list of groups
+    /**
+     * Adds a group to the user's list of groups, if not already present.
+     *
+     * @param userGroup the group to add
+     */
     public void addUserGroup(String userGroup) {
-        if (!(this.userGroups.contains(userGroup))) {
+        if (!this.userGroups.contains(userGroup)) {
             this.userGroups.add(userGroup);
         }
     }
 
+    /**
+     * Sets the user's to-do list by copying an existing ToDoList.
+     *
+     * @param toDoList the ToDoList to set
+     */
     public void setToDoList(ToDoList toDoList) {
         this.toDoList = new ToDoList(toDoList);
     }
 
+    /**
+     * Returns a copy of the user's to-do list.
+     *
+     * @return the user's to-do list
+     */
     public ToDoList getToDoList() {
         return new ToDoList(toDoList);
     }
