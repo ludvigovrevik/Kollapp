4948992fbc27a53a5d1f1805a0a4d11f50340d47
Sang Munn Kim <sangk@stud.ntnu.no>
2024-11-02 13:35:50 +0100

    feat: add TodoListController and TodoListService
    Co-authored-by: Thomas Nordby Sørense <thomanso@stud.ntnu.no>

---
+++ kollapp/api/src/main/java/api/controller/TodoListController.java
@@ -0,0 +1,5 @@
+package com.gr2409.kollapp.api.controller;
+
+public class TodoListController {
+    
+}
---
+++ kollapp/api/src/main/java/api/service/TodoListService.java
@@ -0,0 +1,5 @@
+package com.gr2409.kollapp.api.service;
+
+public class TodoListService {
+    
+}
