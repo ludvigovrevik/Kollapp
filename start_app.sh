fc7a7f4f09aa49565adc44a73cd776c693e9fd16
Sang Munn Kim <sangk@stud.ntnu.no>
2024-11-03 16:04:35 +0100

    refactor: start_app.sh

--- start_app.sh
+++ start_app.sh
@@ -7,8 +7,7 @@ mvn -f ./kollapp clean install -DskipTests
 # Start the Spring Boot application
 # Update "springboot-module" to the name of your Spring Boot module
 echo "Starting Spring Boot application..."
-mvn -f ./kollapp/api spring-boot:run &
-SPRING_PID=$!
+mvn -f ./kollapp/api spring-boot:run
 
 # Wait for Spring Boot to initialize
 # Adjust the sleep duration if needed for your Spring Boot startup time
@@ -19,6 +18,3 @@ sleep 10
 # Update "javafx-module" to the name of your JavaFX module
 echo "Starting JavaFX application..."
 mvn -f ./kollapp/ui javafx:run
-
-# Stop the Spring Boot process after JavaFX application closes
-kill $SPRING_PID
