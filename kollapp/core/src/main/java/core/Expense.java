c61063d6d250c9effbd4265694ab8d4da87c5793
Sang Munn Kim <sangk@stud.ntnu.no>
2024-11-04 17:58:34 +0100

    refactor: separate logic of expense and todo list
    co-authored-by: Ludvig Øvrevik <ludvigho@ntnu.no>

--- kollapp/core/src/main/java/core/Expense.java
+++ kollapp/core/src/main/java/core/Expense.java
@@ -2,53 +2,29 @@ package core;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 /**
  * Represents an expense in the application.
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Expense implements Serializable {
     private String description;
     private double amount;
     private String paidBy;
 
-    /**
-     * Default constructor.
-     */
+    // Default constructor
     public Expense() {
     }
 
-    /**
-     * Constructs a new Expense with specified details.
-     *
-     * @param description The description of the expense.
-     * @param amount      The amount of the expense.
-     * @param paidBy      The username of the person who paid.
-     */
+    // Parameterized constructor
     public Expense(String description, double amount, String paidBy) {
         this.description = description;
         this.amount = amount;
         this.paidBy = paidBy;
     }
 
-    /**
-     * Copy constructor for Expense.
-     *
-     * @param expense The expense to copy.
-     */
-    public Expense(Expense expense) {
-        this.description = expense.description;
-        this.amount = expense.amount;
-        this.paidBy = expense.paidBy;
-    }
-
     // Getters and setters
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
@@ -56,15 +32,13 @@ public class Expense implements Serializable {
     public double getAmount() {
         return amount;
     }
-
     public void setAmount(double amount) {
         this.amount = amount;
     }
-  
+
     public String getPaidBy() {
         return paidBy;
     }
-  
     public void setPaidBy(String paidBy) {
         this.paidBy = paidBy;
     }
--- kollapp/core/src/main/java/core/ToDoList.java
+++ kollapp/core/src/main/java/core/ToDoList.java
@@ -5,17 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * Represents a to-do list that contains multiple tasks and expenses.
+ * Represents a to-do list that contains multiple tasks.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ToDoList implements Serializable {
-    @JsonProperty
     private List<Task> tasks = new ArrayList<>();
-    @JsonProperty
-    private List<Expense> expenses = new ArrayList<>();
 
     /**
      * Default constructor for creating an empty ToDoList.
@@ -24,21 +20,22 @@ public class ToDoList implements Serializable {
     }
 
     /**
-     * Constructs a new ToDoList by copying tasks and expenses from another ToDoList.
+     * Constructs a new ToDoList by copying the tasks from another ToDoList.
      *
-     * @param toDoList the ToDoList to copy.
+     * @param toDoList the ToDoList to copy
      */
     public ToDoList(ToDoList toDoList) {
         for (Task task : toDoList.getTasks()) {
             tasks.add(new Task(task));
         }
-        for (Expense expense : toDoList.getExpenses()) {
-            expenses.add(new Expense(expense));
-        }
     }
 
-    // Task methods remain unchanged
-
+    /**
+     * Adds a task to the to-do list.
+     *
+     * @param newTask the task to add
+     * @throws IllegalArgumentException if the task is null
+     */
     public void addTask(Task newTask) {
         if (newTask == null) {
             throw new IllegalArgumentException("Task cannot be null.");
@@ -46,51 +43,25 @@ public class ToDoList implements Serializable {
         tasks.add(newTask);
     }
 
+    /**
+     * Returns a copy of the list of tasks.
+     *
+     * @return a list of tasks
+     */
     public List<Task> getTasks() {
         return new ArrayList<>(tasks);
     }
 
+    /**
+     * Removes a task from the to-do list.
+     *
+     * @param task the task to remove
+     * @throws IndexOutOfBoundsException if the task is not in the list
+     */
     public void removeTask(Task task) {
         if (!tasks.contains(task)) {
             throw new IndexOutOfBoundsException("Task is not in the list.");
         }
         tasks.remove(task);
-    }
-
-    // New methods for handling expenses
-
-    /**
-     * Adds an expense to the to-do list.
-     *
-     * @param newExpense the expense to add.
-     * @throws IllegalArgumentException if the expense is null.
-     */
-    public void addExpense(Expense newExpense) {
-        if (newExpense == null) {
-            throw new IllegalArgumentException("Expense cannot be null.");
-        }
-        expenses.add(newExpense);
-    }
-
-    /**
-     * Returns a copy of the list of expenses.
-     *
-     * @return a list of expenses.
-     */
-    public List<Expense> getExpenses() {
-        return new ArrayList<>(expenses);
-    }
-
-    /**
-     * Removes an expense from the to-do list.
-     *
-     * @param expense the expense to remove.
-     * @throws IndexOutOfBoundsException if the expense is not in the list.
-     */
-    public void removeExpense(Expense expense) {
-        if (!expenses.contains(expense)) {
-            throw new IndexOutOfBoundsException("Expense is not in the list.");
-        }
-        expenses.remove(expense);
     }
 }
